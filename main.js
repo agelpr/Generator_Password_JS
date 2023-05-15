49abf95f7c9d2d32d541b7cc6d05a2e4603124c7
agelpr94 <angelpuentes21@gmail.com>
2023-05-15 18:16:20 -0400

    Lista la funcion para generar contraseña

---
+++ main.js
@@ -0,0 +1,20 @@
+const passwordBox = document.getElementById("password");
+const lenght = 20;
+const upperCase = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnñopqrstuvwxyz";
+const number = "0123456789"
+const symbol = "!#$%&/()=?¡@*-+¡'}{[]_|"
+const allChrs = upperCase + lowerCase + number + symbol;
+
+    function generatePassword(){
+        let password = "";
+        password += upperCase[Math.floor(Math.random() * upperCase.length)];
+        password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+        password += number[Math.floor(Math.random() * number.length)];
+        password += symbol[Math.floor(Math.random() * symbol.length)];
+
+        while(lenght > password.length){
+            password += allChrs[Math.floor(Math.random() * allChrs.length)];
+        }
+        passwordBox.value = password;
+        }
